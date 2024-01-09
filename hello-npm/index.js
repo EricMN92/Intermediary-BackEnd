4c252a81092ed083969e9b44c9abaabc54032c73
Eric Nomura <eric.mamoru@gmail.com>
2024-01-09 17:59:49 -0300

    Adicionando exemplo de npm

---
+++ hello-npm/index.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+
+inquirer.prompt(
+    [
+        {
+            type: 'input',
+            name: 'nome',
+            message: 'Qual é o seu nome?',
+        },
+        {
+            type: 'list',
+            name: 'idade',
+            message: 'Qual é a sua idade?',
+            choices: [
+                '29 a 39',
+                '40 a 50',
+                '50+'
+            ]
+        }
+    ]
+).then((answer) => {
+    console.log(
+        "Oi " + answer.nome +
+        " com " + answer.idade + " anos de vida!"
+    )
+});
